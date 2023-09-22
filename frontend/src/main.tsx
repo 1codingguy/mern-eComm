1f264bf8ffe9ef94842c861abf638df9e2c6b226
1codingguy <1codingguy@gmail.com>
2023-09-23 01:01:01 +0700

    product details page, finish frontend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -10,11 +10,13 @@ import App from './App.tsx'
 import './assets/styles/bootstrap.custom.css'
 import './assets/styles/index.css'
 import HomeScreen from './screens/HomeScreen.tsx'
+import ProductScreen from './screens/ProductScreen.tsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
-      <Route index={true} path='/' element={<HomeScreen />} />
+      <Route path='/' element={<HomeScreen />} index={true} />
+      <Route path='/product/:id' element={<ProductScreen />} />
     </Route>
   )
 )
