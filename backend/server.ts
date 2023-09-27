d18a629828a17e431f3dea53f33b91e246e7ab4a
1codingguy <1codingguy@gmail.com>
2023-09-28 00:09:48 +0700

    express server initial setup and simple test routes

--- backend/server.ts
+++ backend/server.ts
@@ -1,5 +1,22 @@
-import express from "express";
+import express from 'express'
+import products from './data/products.js'
 
 const port = 6900
 const app = express()
 
+app.get('/', (req, res) => {
+  res.send('API is running')
+})
+
+app.listen(port, () => {
+  console.log(`Server running in port ${port}`)
+})
+
+app.get('/api/products', (req, res) => {
+  res.json(products)
+})
+
+app.get('/api/products/:id', (req, res) => {
+  const product = products.find(({ _id }) => _id === req.params.id)
+  return product ? res.json(product) : res.send('Product not found')
+})
