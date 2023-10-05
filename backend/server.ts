addc5042c2d7f25f86ea7b282f189ddc6b72b074
1codingguy <1codingguy@gmail.com>
2023-10-05 18:18:09 +0700

    fetch data from mongoDB to the frontend

--- backend/server.ts
+++ backend/server.ts
@@ -1,7 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
-// import products from './data/products.js'
 import connectDB from './config/db.js'
+import productRoutes from "./routes/productRoutes.js"
 
 dotenv.config()
 
@@ -15,15 +15,10 @@ app.get('/', (req, res) => {
   res.send('API is running')
 })
 
+// 
+app.use('/api/products', productRoutes)
+
 app.listen(port, () => {
   console.log(`Server running in port ${port}`)
 })
 
-// app.get('/api/products', (req, res) => {
-//   res.json(products)
-// })
-
-// app.get('/api/products/:id', (req, res) => {
-//   const product = products.find(({ _id }) => _id === req.params.id)
-//   return product ? res.json(product) : res.send('Product not found')
-// })
