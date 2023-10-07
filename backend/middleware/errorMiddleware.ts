43c65ca03da75f41a7f13561ac77935d947a367f
1codingguy <1codingguy@gmail.com>
2023-10-07 17:39:42 +0700

    custom middleware functions

---
+++ backend/middleware/errorMiddleware.ts
@@ -0,0 +1,31 @@
+import { Request, Response, NextFunction } from 'express'
+import { Error as MongooseError } from 'mongoose'
+
+interface CastError extends MongooseError {
+  kind: string
+}
+
+type MiddleWare = [req: Request, res: Response, next: NextFunction]
+type NotFound = (...params: MiddleWare) => void
+type ErrorHandler = (err: Error, ...params: MiddleWare) => void
+
+export const notFound: NotFound = (req, res, next) => {
+  const error = new Error(`Not found - ${req.originalUrl}`)
+  res.status(404)
+  next(error)
+}
+
+export const errorHandler: ErrorHandler = (err, req, res, next) => {
+  let statusCode = res.statusCode === 200 ? 500 : res.statusCode
+  let message = err.message
+
+  if (err.name === 'CastError' && (err as CastError).kind === 'ObjectId') {
+    message = 'Resource not found'
+    statusCode = 404
+  }
+
+  res.status(statusCode).json({
+    message,
+    stack: process.env.NODE_ENV === 'production' ? '🥞' : err.stack,
+  })
+}
