c29c9749d183bc0d87daddab7f5be442bd5f8e49
1codingguy <1codingguy@gmail.com>
2023-10-08 16:26:14 +0700

    product detail from redux

--- frontend/src/screens/HomeScreen.tsx
+++ frontend/src/screens/HomeScreen.tsx
@@ -1,32 +1,32 @@
-import { useEffect, useState } from 'react'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
-import axios from 'axios'
-import { ProductType } from '../../../backend/models/productModel'
+import { useGetProductsQuery } from '../slices/productsApiSlice'
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState<ProductType[] | []>([])
-
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get<ProductType[]>('/api/products')
-      setProducts(data)
-    }
-    fetchProducts()
-  }, [])
-
+  // TODO: deal with products type later
+  const { data: products, isLoading, error } = useGetProductsQuery()
+  console.log(isLoading, error)
+  console.log(products)
   return (
     <>
-      <h1>Latest Products</h1>
-      <Row>
-        {products.map(product => {
-          return (
-            <Col sm={12} md={6} lg={4} xl={3} key={String(product._id)}>
-              <Product product={product} />
-            </Col>
-          )
-        })}
-      </Row>
+      {isLoading ? (
+        <h2>Loading</h2>
+      ) : error ? (
+        <div>{error?.data?.message || error.error}</div>
+      ) : (
+        <>
+          <h1>Latest Products</h1>
+          <Row>
+            {products.map(product => {
+              return (
+                <Col sm={12} md={6} lg={4} xl={3} key={String(product._id)}>
+                  <Product product={product} />
+                </Col>
+              )
+            })}
+          </Row>
+        </>
+      )}
     </>
   )
 }
