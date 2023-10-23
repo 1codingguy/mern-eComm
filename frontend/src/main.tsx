b74614c0392b8b7c618bdb5e1cf47a0feb10db59
--global <1codingguy@gmail.com>
2023-10-23 10:28:42 +0700

    login screen

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -14,6 +14,7 @@ import './assets/styles/index.css'
 import HomeScreen from './screens/HomeScreen.tsx'
 import ProductScreen from './screens/ProductScreen.tsx'
 import CartScreen from './screens/CartScreen.tsx'
+import LoginScreen from './screens/LoginScreen.tsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -21,6 +22,7 @@ const router = createBrowserRouter(
       <Route path='/' element={<HomeScreen />} index={true} />
       <Route path='/product/:id' element={<ProductScreen />} />
       <Route path='/cart' element={<CartScreen />} />
+      <Route path='/login' element={<LoginScreen />} />
     </Route>
   )
 )
