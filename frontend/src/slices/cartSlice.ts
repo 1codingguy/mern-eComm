4559d75d23480227389499ed221fffc95bcd4198
--global <1codingguy@gmail.com>
2023-10-24 09:47:54 +0700

    shipping screen

--- frontend/src/slices/cartSlice.ts
+++ frontend/src/slices/cartSlice.ts
@@ -15,10 +15,18 @@ export interface CartState {
   shippingPrice: string
   taxPrice: string
   totalPrice: string
+  shippingAddress: {
+    address: string
+    city: string
+    postalCode: string
+    country: string
+  }
 }
 
 const initialState: CartState =
-  localData !== null ? JSON.parse(localData) : { cartItems: [] }
+  localData !== null
+    ? JSON.parse(localData)
+    : { cartItems: [], shippingAddress: {}, paymentMethod: 'Paypal' }
 
 const cartSlice = createSlice({
   name: 'cart',
@@ -44,13 +52,19 @@ const cartSlice = createSlice({
       return updateCart(state)
     },
     removeFromCart: (state, action) => {
-      state.cartItems = state.cartItems.filter(cartItem => cartItem._id !== action.payload.id)
-
+      state.cartItems = state.cartItems.filter(
+        cartItem => cartItem._id !== action.payload.id
+      )
       return updateCart(state)
-    }
+    },
+    saveShippingAddress: (state, action) => {
+      state.shippingAddress = action.payload
+      return updateCart(state)
+    },
   },
 })
 
-export const { addToCart, removeFromCart } = cartSlice.actions
+export const { addToCart, removeFromCart, saveShippingAddress } =
+  cartSlice.actions
 
 export default cartSlice.reducer
