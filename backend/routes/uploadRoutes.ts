16771a2c02d394c9ca54529488419e130552b0d8
--global <1codingguy@gmail.com>
2023-11-07 22:48:28 +0800

    multer and image upload endpoint

---
+++ backend/routes/uploadRoutes.ts
@@ -0,0 +1,56 @@
+import path from 'path'
+import express from 'express'
+import multer from 'multer'
+const router = express.Router()
+
+const storage = multer.diskStorage({
+  // cb is the callback
+  destination(req, file, cb) {
+    // null is for error, we don't have an error, so put a null here
+    // 'uploads/' is the folder we want to upload to
+    cb(null, 'uploads/')
+  },
+  // describe how the file should be named and formatted
+  filename(req, file, cb) {
+    cb(
+      null,
+      `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
+    )
+  },
+})
+
+const checkFileType = (
+  file: Express.Multer.File,
+  cb: (error: Error | null, result: boolean) => void
+) => {
+  const filetypes = /jpg|jpeg|png/
+  // test() tests if the file extension matches any of the filetypes
+
+  // extname() returns the extension of the path
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase())
+
+  // mimetype is the file type
+  const mimetype = filetypes.test(file.mimetype)
+
+  if (extname && mimetype) {
+    return cb(null, true)
+  } else {
+    // null is for error
+    cb(new Error('Images only!'), false)
+    // null is for error
+    cb('Images only!')
+  }
+}
+
+const upload = multer({
+  storage,
+})
+
+router.post('/', upload.single('image'), (req, res) => {
+  res.send({
+    image: `/${req?.file?.path}`,
+    message: 'Image uploaded successfully',
+  })
+})
+
+export default router
--- backend/server.ts
+++ backend/server.ts
@@ -1,9 +1,11 @@
+import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
-import productRoutes from "./routes/productRoutes.js"
-import orderRoutes from "./routes/orderRoutes.js"
-import userRoutes from "./routes/userRoutes.js"
+import productRoutes from './routes/productRoutes.js'
+import orderRoutes from './routes/orderRoutes.js'
+import userRoutes from './routes/userRoutes.js'
+import uploadRoutes from './routes/uploadRoutes.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import cookieParser from 'cookie-parser'
 
@@ -25,14 +27,21 @@ app.get('/', (req, res) => {
   res.send('API is running')
 })
 
-// 
+//
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
+app.use('/api/upload', uploadRoutes)
 
 app.get('/api/config/paypal', (req, res) =>
   res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
 )
+
+// __dirname is not available in ES6 module, so we need to use path.resolve()
+const __dirname = path.resolve() // __dirname is the current directory
+
+// make the uploads folder static
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 app.use(notFound)
 app.use(errorHandler)
@@ -40,4 +49,3 @@ app.use(errorHandler)
 app.listen(port, () => {
   console.log(`Server running in port ${port}`)
 })
-
